393e879cc8f2d2cddc0c05fbab84336fa6fc6b2e
Stuart Bruce <programming@brucey.me.uk>
2024-05-06 11:28:58 +0100

    Error if this is used outside a class.

--- jlox/Resolver.java
+++ jlox/Resolver.java
@@ -9,6 +9,7 @@ public class Resolver implements  Expr.Visitor<Void>, Stmt.Visitor<Void> {
     private final Interpreter interpreter;
     private final Stack<Map<String, Boolean>> scopes = new Stack<>();
     private FunctionType currentFunction = FunctionType.NONE;
+    private ClassType currentClass = ClassType.NONE;
 
     Resolver(Interpreter interpreter) {
         this.interpreter = interpreter;
@@ -18,6 +19,11 @@ public class Resolver implements  Expr.Visitor<Void>, Stmt.Visitor<Void> {
         NONE,
         FUNCTION,
         METHOD,
+    }
+
+    private enum ClassType {
+        NONE,
+        CLASS,
     }
 
     void resolve(List<Stmt> statements) {
@@ -36,6 +42,9 @@ public class Resolver implements  Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitClassStmt(Stmt.Class stmt) {
+        ClassType enclosingClass = currentClass;
+        currentClass = ClassType.CLASS;
+
         declare(stmt.name);
         define(stmt.name);
 
@@ -48,6 +57,8 @@ public class Resolver implements  Expr.Visitor<Void>, Stmt.Visitor<Void> {
         }
 
         endScope();
+
+        currentClass = enclosingClass;
         return null;
     }
 
@@ -165,7 +176,11 @@ public class Resolver implements  Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitThisExpr(Expr.This expr) {
-        resolveLocal(expr, expr.keyword);
+        if (currentClass == ClassType.NONE) {
+            Lox.error(expr.keyword, "Can't use 'this' keyword outside of a class.");
+        } else {
+            resolveLocal(expr, expr.keyword);
+        }
         return null;
     }
 
