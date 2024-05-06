971175e986f6990775dd45360d1693610e0f6b40
Stuart Bruce <programming@brucey.me.uk>
2024-05-06 15:41:22 +0100

    Handle calling super outside of subclass contexts.

--- jlox/Resolver.java
+++ jlox/Resolver.java
@@ -25,6 +25,7 @@ public class Resolver implements  Expr.Visitor<Void>, Stmt.Visitor<Void> {
     private enum ClassType {
         NONE,
         CLASS,
+        SUBCLASS,
     }
 
     void resolve(List<Stmt> statements) {
@@ -53,6 +54,7 @@ public class Resolver implements  Expr.Visitor<Void>, Stmt.Visitor<Void> {
             if (stmt.name.lexeme.equals(stmt.superclass.name.lexeme)) {
                 Lox.error(stmt.superclass.name, "A class can't inherit from itself.");
             } else {
+                currentClass = ClassType.SUBCLASS;
                 resolve(stmt.superclass);
             }
 
@@ -202,6 +204,12 @@ public class Resolver implements  Expr.Visitor<Void>, Stmt.Visitor<Void> {
 
     @Override
     public Void visitSuperExpr(Expr.Super expr) {
+        if (currentClass == ClassType.NONE) {
+            Lox.error(expr.keyword, "Can't use 'super' outside of a class.");
+        } else if (currentClass != ClassType.SUBCLASS) {
+            Lox.error(expr.keyword, "Can't use 'super' in a class with no superclass.");
+        }
+
         resolveLocal(expr, expr.keyword);
         return null;
     }
