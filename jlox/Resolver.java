b23d2343437a903954ff8098e326fb1a875971e1
Stuart Bruce <programming@brucey.me.uk>
2024-05-06 11:24:31 +0100

    Class this keyword.

--- jlox/Resolver.java
+++ jlox/Resolver.java
@@ -39,10 +39,15 @@ public class Resolver implements  Expr.Visitor<Void>, Stmt.Visitor<Void> {
         declare(stmt.name);
         define(stmt.name);
 
+        beginScope();
+        scopes.peek().put("this", true);
+
         for (Stmt.Function method: stmt.methods) {
             FunctionType declaration = FunctionType.METHOD;
             resolveFunction(method, declaration);
         }
+
+        endScope();
         return null;
     }
 
@@ -155,6 +160,12 @@ public class Resolver implements  Expr.Visitor<Void>, Stmt.Visitor<Void> {
     public Void visitSetExpr(Expr.Set expr) {
         resolve(expr.object);
         resolve(expr.value);
+        return null;
+    }
+
+    @Override
+    public Void visitThisExpr(Expr.This expr) {
+        resolveLocal(expr, expr.keyword);
         return null;
     }
 
