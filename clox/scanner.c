110a6644ac63278e8d3e0316003652125a054b65
Stuart Bruce <programming@brucey.me.uk>
2024-07-01 19:23:35 +0100

    Tokeniser base work.

--- clox/scanner.c
+++ clox/scanner.c
@@ -21,3 +21,33 @@ void initScanner(const char *source) {
     scanner.current = source;
     scanner.line = 1;
 }
+
+static bool isAtEnd() {
+    return *scanner.current == '\0';
+}
+
+static Token mkToken(TokenType type) {
+    Token token;
+    token.type = type;
+    token.start = scanner.start;
+    token.length = (int)(scanner.current - scanner.start);
+    token.line = scanner.line;
+    return token;
+}
+
+static Token errorToken(const char *msg) {
+    Token token;
+    token.type = TOKEN_ERROR;
+    token.start = msg;
+    token.length = (int)strlen(msg);
+    token.line = scanner.line;
+    return token;
+}
+
+Token scanToken() {
+    scanner.start = scanner.current;
+
+    if (isAtEnd()) return mkToken(TOKEN_EOF);
+
+    return errorToken("Unexpected characters.");
+}
