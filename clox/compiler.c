7cdbaa83e7cd27f385853476db600f7f1a0cd8ce
Stuart Bruce <programming@brucey.me.uk>
2024-07-01 19:05:19 +0100

    WIP: Setup frontend and compiler.

---
+++ clox/compiler.c
@@ -0,0 +1,11 @@
+//
+// Created by Brucey on 01/07/2024.
+//
+
+#include "common.h"
+#include "compiler.h"
+#include "scanner.h"
+
+void compile(const char *source) {
+    initScanner(source);
+}
---
+++ clox/compiler.h
@@ -0,0 +1,10 @@
+//
+// Created by Brucey on 01/07/2024.
+//
+
+#ifndef COMPILER_H
+#define COMPILER_H
+
+void compile(const char *source);
+
+#endif //COMPILER_H
