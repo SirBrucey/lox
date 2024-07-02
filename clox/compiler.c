3ef432cf68d0bbe981eb59f012b8c1c9632da402
Stuart Bruce <programming@brucey.me.uk>
2024-07-02 12:33:20 +0100

    Parse tokens and error handling.

--- clox/compiler.c
+++ clox/compiler.c
@@ -3,26 +3,74 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char *source) {
-    initScanner(source);
-    // Temp code to get scanner moving.
-    int line = -1;
-    for (;;) {
-        Token token = scanToken();
+typedef struct {
+    Token current;
+    Token previous;
+    bool hadError;
+    bool panicMode;
+} Parser;
 
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        } else {
-            printf("   | ");
-        }
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+Parser parser;
 
-        if (token.type == TOKEN_EOF) break;
+static void errorAt(Token *token, const char *msg) {
+    if (parser.panicMode) return;
+    parser.panicMode = true;
+    fprintf(stderr, "[line %d] Error", token->line);
+
+    if (token->type == TOKEN_EOF) {
+        fprintf(stderr, " at end");
+    } else if (token->type == TOKEN_ERROR) {
+        // Nothing
+    } else {
+        fprintf(stderr, " at '%.*s'", token->length, token->start);
     }
+
+    fprintf(stderr, " %s\n", msg);
+    parser.hadError = true;
+}
+
+static void error(const char *msg) {
+    errorAt(&parser.previous, msg);
+}
+
+static void errorAtCurrent(const char *msg) {
+    errorAt(&parser.current, msg);
+}
+
+static void advance(void) {
+    parser.previous = parser.current;
+
+    for (;;) {
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
+
+        errorAtCurrent(parser.current.start);
+    }
+}
+
+static void consume(const TokenType type, const char *msg) {
+    if (parser.current.type == type) {
+        advance();
+        return;
+    }
+
+    errorAtCurrent(msg);
+}
+
+void compile(const char *source, Chunk *chunk) {
+    initScanner(source);
+
+    parser.hadError = false;
+    parser.panicMode = false;
+
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression");
+    return !parser.hadError;
 }
