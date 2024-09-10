fd7f5efa2c0b2a39b42ca0535000af62405d6c73
Stuart Bruce <programming@brucey.me.uk>
2024-09-10 16:46:40 +0100

    While statements.

--- clox/compiler.c
+++ clox/compiler.c
@@ -132,6 +132,16 @@ static void emitBytes(const uint8_t byte1, const uint8_t byte2) {
     emitByte(byte2);
 }
 
+static void emitLoop(const int loopStart) {
+    emitByte(OP_LOOP);
+
+    const int offset = currentChunk()->count - loopStart + 2;
+    if (offset > UINT16_MAX) error("Loop body too large.");
+
+    emitByte((offset >> 8) & 0xff);
+    emitByte(offset & 0xff);
+}
+
 static int emitJump(const uint8_t instruction) {
     emitByte(instruction);
     emitByte(0xff);
@@ -454,6 +464,22 @@ static void printStatement(void) {
     emitByte(OP_PRINT);
 }
 
+static void whileStatement(void) {
+    int loopStart = currentChunk()->count;
+
+    consume(TOKEN_LEFT_PAREN, "Expect '(' after 'while'.");
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after condition.");
+
+    const int exitJump = emitJump(OP_JUMP_IF_FALSE);
+    emitByte(OP_POP);
+    statement();
+    emitLoop(loopStart);
+
+    patchJump(exitJump);
+    emitByte(OP_POP);
+}
+
 static void syncronize(void) {
     parser.panicMode = false;
 
@@ -492,6 +518,8 @@ static void statement(void) {
         printStatement();
     } else if (match(TOKEN_IF)) {
         ifStatement();
+    } else if (match(TOKEN_WHILE)) {
+        whileStatement();
     } else if (match(TOKEN_LEFT_BRACE)) {
         beginScope();
         block();
